8b5d3cdedc8cf44ce4382671bd87f073a79551f1
Tony Bowden <tony@tmtm.com>
2017-09-05 06:19:08 +0100

    rewrite for new tabular layout

--- scraper.rb
+++ scraper.rb
@@ -7,7 +7,7 @@ hu_names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://hu.wikipedia.org/wiki/2014%E2%80%932018_k%C3%B6z%C3%B6tti_magyar_orsz%C3%A1ggy%C5%B1l%C3%A9si_k%C3%A9pvisel%C5%91k_list%C3%A1ja',
   after: '//h2[span[@id="K.C3.A9pvisel.C5.91k"]]',
   before: '//h2[span[@id="Nemzetis.C3.A9gi_sz.C3.B3sz.C3.B3l.C3.B3k"]]',
-  xpath: './/ul/li/a[not(@class="new")]/@title',
+  xpath: './/table//tr[td]//td[1]//a[not(@class="new")]/@title',
 )
 en_names = WikiData::Category.new( 'Category:Members of the National Assembly of Hungary (2014–18)', 'en').member_titles
 
